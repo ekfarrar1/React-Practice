94ce792ac49be5b3881ff7bf2fdedccc335c5500
ekfarrar <efarrar@vols.utk.edu>
2024-01-24 16:13:22 -0500

    trying to fix 'npm start' no longer working

---
+++ React-Practice/progress-bar/src/input-box/input-box.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+function Inputbox(){
+    return (
+    <form>
+        <label>input percentage: </label>
+        <input name='percentage'/>
+    </form>
+    )
+}
+
+export default Inputbox
