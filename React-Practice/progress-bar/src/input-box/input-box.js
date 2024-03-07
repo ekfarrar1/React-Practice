0cadcf5ee63dedb1e483f8ae23d5d0ea376fe911
ekfarrar <efarrar@vols.utk.edu>
2024-03-07 14:46:34 -0500

    removed extra folder

--- React-Practice/progress-bar/src/input-box/input-box.js
+++ React-Practice/progress-bar/src/input-box/input-box.js
@@ -1,12 +1,11 @@
-import React from 'react'
-
+import React from 'react';
 
 function Inputbox(){
     return (
-    <form>
-        <label>input percentage: </label>
-        <input name='percentage'/>
-    </form>
+        <form>
+            <label>input percentage: </label>
+            <input name='percentage' type='text' pattern='^(0|[1-9][0-9]?|100)$'/>
+        </form>
     )
 }
 
--- React-Practice/show-hide-button/src/App.js
+++ React-Practice/show-hide-button/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import { useState } from "react";
 
 function App() {
-  const [showing, setShow] = useState(1);
+  const [showing, setShow] = useState(0);
 
   function handleClick() {
     setShow((showing) => !showing);
